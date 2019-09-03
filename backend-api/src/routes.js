56f730409165837625335a5c81c5c1965eb49033
Junnnior <juniorfoxld@hotmail.com>
2019-09-03 19:31:20 -0300

    conectando mongodb

--- backend-api/src/routes.js
+++ backend-api/src/routes.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const VendorController = require('./controllers/VendorController');
 
 const routes = express.Router();
 
+
+// router start
 routes.get('/', (req, res) => {
     return res.send("Ola");
 })
+
+// creating vendor
+routes.post('/vendors', VendorController.store);
 
 module.exports = routes;
