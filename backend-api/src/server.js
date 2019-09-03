f213c4a11eece61b1e56c0d17c13e3a0c698525c
Junnnior <juniorfoxld@hotmail.com>
2019-09-02 22:02:22 -0300

    criando arquivo de rotas

--- backend-api/src/server.js
+++ backend-api/src/server.js
@@ -1,9 +1,6 @@
 const express = require('express');
+const routes = require('./routes');
 
 const server = express();
-
-server.get('/', (req, res) => {
-    return res.send("Ola");
-})
-
+server.use(routes);
 server.listen(3333);
